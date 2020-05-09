70cab1f18aa551001295b6653547a6209ffcc191
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-09 21:05:47 +0300

    finished createFighterPreview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,17 +7,63 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    fighterElement.style.height = '400px';
+    fighterElement.style.backgroundColor = '#FFE387';
+    fighterElement.style.border = '6px solid #FFCA6E';
+
+    const fighterName = createElement({
+      tagName: 'div',
+      className: 'fighter-preview___name',
+    });
+    fighterName.innerText = fighter.name;
+
+    const fighterImg = createElement({
+      tagName: 'img',
+      className: 'fighter-preview___img',
+    });
+    fighterImg.src = fighter.source;
+
+    const fighterDesc = createElement({
+      tagName: 'div',
+      className: 'fighter-preview___description',
+    });
+
+    const fighterHealth = createElement({
+      tagName: 'div',
+      className: 'fighter-preview___data',
+    });
+    fighterHealth.innerText = 'Health: ' + fighter.health;
+    fighterDesc.append(fighterHealth);
+
+    const fighterAttack = createElement({
+      tagName: 'div',
+      className: 'fighter-preview___data',
+    });
+    fighterAttack.innerText = 'Attack: ' + fighter.attack;
+    fighterDesc.append(fighterAttack);
+
+    const fighterDefense = createElement({
+      tagName: 'div',
+      className: 'fighter-preview___data',
+    });
+    fighterDefense.innerText = 'Defense: ' + fighter.defense;
+    fighterDesc.append(fighterDefense);
+
+    fighterElement.append(fighterName);
+    fighterElement.append(fighterImg);
+    fighterElement.append(fighterDesc);
+  }
 
   return fighterElement;
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
+  const attributes = {
+    src: source,
     title: name,
-    alt: name 
+    alt: name,
   };
   const imgElement = createElement({
     tagName: 'img',
