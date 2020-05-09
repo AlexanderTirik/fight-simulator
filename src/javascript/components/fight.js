5ebf97a434db3989375bb9d86ef44b032a4345cc
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-10 00:29:02 +0300

    Added fight system without crits

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -2,7 +2,53 @@ import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
-    // resolve the promise with the winner when fight is over
+    let pressed = new Set();
+
+    const firstHealthBar = document.getElementById('left-fighter-indicator');
+    const secondHealthBar = document.getElementById('right-fighter-indicator');
+
+    const firstHealth = firstFighter.health;
+    const secondHealth = secondFighter.health;
+
+    document.addEventListener('keydown', function (event) {
+      if (
+        event.code === controls.PlayerOneAttack &&
+        !pressed.has(controls.PlayerOneBlock) &&
+        !pressed.has(controls.PlayerTwoBlock) &&
+        !event.repeat
+      ) {
+        secondFighter.health -= getDamage(firstFighter, secondFighter);
+        const secondPercentHealth =
+          secondFighter.health > 0 ? Math.floor((secondFighter.health / secondHealth) * 100) : 0;
+        secondHealthBar.style.width = `${secondPercentHealth}%`;
+        if (secondFighter.health <= 0) resolve(firstFighter)
+      }
+
+      if (
+        event.code === controls.PlayerTwoAttack &&
+        !pressed.has(controls.PlayerTwoBlock) &&
+        !pressed.has(controls.PlayerOneBlock) &&
+        !event.repeat
+      ) {
+        firstFighter.health -= getDamage(secondFighter, firstFighter);
+        const firstPercentHealth = firstFighter.health > 0 ? Math.floor((firstFighter.health / firstHealth) * 100) : 0;
+        firstHealthBar.style.width = `${firstPercentHealth}%`;
+        if (firstFighter.health <= 0) resolve(secondFighter)
+      }
+
+      if (event.code === controls.PlayerOneBlock && !event.repeat) {
+        pressed.add(event.code);
+      }
+
+      if (event.code === controls.PlayerTwoBlock && !event.repeat) {
+        pressed.add(event.code);
+      }
+    });
+    document.addEventListener('keyup', function (event) {
+      if (event.code === controls.PlayerOneBlock || event.code === controls.PlayerTwoBlock) {
+        pressed.delete(event.code);
+      }
+    });
   });
 }
 
